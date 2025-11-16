fda5a2cac344ebd4ef35ccfbf163be9850af159b
Gitali03 <aonermtk@gmail.com>
2025-11-16 16:19:26 +0300

    modal

---
+++ src/components/Cart.jsx
@@ -0,0 +1,31 @@
+import { useContext } from "react";
+import Modal from "./UI/Modal";
+import CartContext from "../store/CartContext";
+import { currecyFormatter } from "../util/formatting";
+import Button from "./UI/Button";
+
+export default function Cart() {
+  const cartCtx = useContext(CartContext);
+
+  const cartTotal = cartCtx.items.reduce(
+    (totalPrice, item) => totalPrice + item.quantity * item.price,
+    0
+  );
+  return (
+    <Modal className="cart" open={true}>
+      <h2>Your Cart</h2>
+      <ul>
+        {cartCtx.items.map((item) => (
+          <li key={item.id}>
+            {item.name} - {item.quantity}
+          </li>
+        ))}
+      </ul>
+      <p className="cart-total">{currecyFormatter.format(cartTotal)}</p>
+      <p className="modal-actions">
+        <Button textOnly>Close</Button>
+        <Button> Go to Checkout</Button>
+      </p>
+    </Modal>
+  );
+}
